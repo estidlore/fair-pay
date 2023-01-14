45cef4ee9cf39754aee81897206aeacf7cb37929
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-01-13 23:05:12 -0500

    feat: #2 add react ts template
    - Add React with Typescript and Webpack
    - Add EsLint and Prettier with custom rules
    - Add Scss support for styles
    - Add Jest with media files mock and styles mapping
    - Add basic vscode settings

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import type { FC } from "react";
+import React from "react";
+
+const App: FC = (): JSX.Element => {
+  return (
+    <div>
+      <p>{"Fair Pay"}</p>
+    </div>
+  );
+};
+
+export { App };
