57737acb4b20b31eea3ceb9c86d528bee16c823c
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-01-15 22:35:01 -0500

    feat: #11 convert inputs into controlled
    - Convert Input and Select into controlled components
    - Add className prop to Card
    - Add type prop to Input

--- src/components/Input/types.ts
+++ src/components/Input/types.ts
@@ -2,7 +2,7 @@ import type { InputHTMLAttributes } from "react";
 
 type InputProps = Pick<
   InputHTMLAttributes<HTMLInputElement>,
-  "name" | "onChange" | "placeholder"
+  "name" | "onChange" | "placeholder" | "type" | "value"
 >;
 
 export type { InputProps };
--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,6 +1,7 @@
 import "./styles.scss";
 
-import type { FC } from "react";
+import type { ChangeEvent, FC } from "react";
+import { useCallback, useState } from "react";
 import React from "react";
 
 import type { SelectProps } from "./types";
@@ -8,10 +9,26 @@ import type { SelectProps } from "./types";
 const Select: FC<SelectProps> = ({
   children,
   name,
-  onChange
+  onChange,
+  value = ""
 }: SelectProps): JSX.Element => {
+  const [controlledValue, setControlledValue] = useState(value);
+
+  const handleChange = useCallback(
+    (ev: ChangeEvent<HTMLSelectElement>) => {
+      onChange?.(ev);
+      setControlledValue(ev.target.value);
+    },
+    [onChange, setControlledValue]
+  );
+
   return (
-    <select className={"select"} name={name} onChange={onChange}>
+    <select
+      className={"select"}
+      name={name}
+      onChange={handleChange}
+      value={controlledValue}
+    >
       {children}
     </select>
   );
