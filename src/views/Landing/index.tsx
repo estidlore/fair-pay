48c215fe3fa968617ce49f56f332479df5a7bfcd
Estid Felipe Lozano Reyes <36607963+estidlore@users.noreply.github.com>
2023-01-16 20:56:59 -0500

    feat: #20 set taking order flow (#31)
    - Add takingOrder state to take order by order
    - Show subtotal price in OrderItems

--- src/views/Landing/index.tsx
+++ src/views/Landing/index.tsx
@@ -1,14 +1,16 @@
 import "./styles.scss";
 
 import type { FC } from "react";
-import { createContext } from "react";
-import React from "react";
+import React, { createContext, useCallback, useReducer } from "react";
 
+import { Button } from "components/Button";
 import { Card } from "components/Card";
 import { Select } from "components/Select";
+import type { Order } from "types";
 import { useArrayReducer } from "utils/hooks/arrayReducer";
 
 import { OrderItems } from "./OrderItems";
+import { TakeOrder } from "./TakeOrder";
 import type { TableContextValue } from "./types";
 
 const tableInitialValue: TableContextValue = {
@@ -24,12 +26,21 @@ const Landing: FC = (): JSX.Element => {
   const [orders, addOrder, deleteOrder, editOrder] = useArrayReducer(
     tableInitialValue.orders
   );
+  const [takingOrder, toggleTakingOrder] = useReducer((val) => !val, false);
+
+  const handleAddOrder = useCallback(
+    (order: Order) => {
+      addOrder(order);
+      toggleTakingOrder();
+    },
+    [addOrder, toggleTakingOrder]
+  );
 
   return (
     <div>
       <TableContext.Provider
         value={{
-          addOrder,
+          addOrder: handleAddOrder,
           deleteOrder,
           editOrder,
           id: 1,
@@ -57,6 +68,11 @@ const Landing: FC = (): JSX.Element => {
           )}
         </div>
         <hr />
+        {takingOrder ? (
+          <TakeOrder onCancel={toggleTakingOrder} />
+        ) : (
+          <Button onClick={toggleTakingOrder}>{"Add order"}</Button>
+        )}
       </TableContext.Provider>
     </div>
   );
